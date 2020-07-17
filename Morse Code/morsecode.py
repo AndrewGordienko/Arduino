ffa7181cba384612c2f4b53ec35ba0eb8207452c
Andrew Gordienko <60705784+AndrewGordienko@users.noreply.github.com>
2020-07-17 15:41:12 -0400

    Update morsecode.py

--- Morse Code/morsecode.py
+++ Morse Code/morsecode.py
@@ -7,8 +7,8 @@ morsecode = {"a" : ".-", "b" : "-...", "c" : "-.-.", "d" : "-..", "e" : ".",
 "r" : ".-.", "s" : "...", "t" : "-", "u" : "..-", "v" : "...-", "w" : ".--",
 "x" : "-..-", "y" : "-.--", "z" : "--.."}
 
-ourstring  = "abc"
-temp = ""
+ourstring  = ""  # any string that the user wants
+temp = ""  # leave blank
 
 for i in range(len(ourstring)):
     for key, value in morsecode.items():
@@ -17,11 +17,14 @@ for i in range(len(ourstring)):
 
 
 for i in range(len(temp)):
-    with serial.Serial('COM3', 9800, timeout=1) as ser:
+    with serial.Serial('COM3', 9800, timeout=1) as ser:  # change COM3 depending on the port your arduino is plugged into
         print (temp[i])
         if temp[i] == "-":
             ser.write(b'H')
-            time.sleep(1)
+            time.sleep(2)
+            ser.write(b'L')
+
         else:
             ser.write(b'H')
-            time.sleep(2)
+            time.sleep(1)
+            ser.write(b'L')
